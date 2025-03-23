a96aa95c3ed92018766531221906adbedbdc3363
Luverk1d <57453642+Arnfourm@users.noreply.github.com>
2025-03-23 22:40:23 +0300

    Add files via upload

--- NotionClone/src/main/java/org/notionclone/MainController.java
+++ NotionClone/src/main/java/org/notionclone/MainController.java
@@ -1,14 +1,24 @@
 package org.notionclone;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane notePane;
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private void initialize() {
+        closeButton.setOnAction(event -> notePane.setVisible(false));
+    }
+
+    @FXML
+    private void openNotePanel() {
+        notePane.setVisible(true);
     }
 }
